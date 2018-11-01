22dc3cbd6ac5a3dceca934b41ffdc6dd097c4929
Swapnil Sarwe <swapnilsarwe@gmail.com>
2018-11-01 18:14:22 +0530

    updating the namespace

--- src/ICNDb/Client.php
+++ src/ICNDb/Client.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ICNDb;
+namespace Swapnilsarwe;
 
 class Client
 {
--- tests/ICNDbTest.php
+++ tests/ICNDbTest.php
@@ -1,78 +1,78 @@
 <?php
 
-use ICNDb\Client;
+use Swapnilsarwe\Client;
 
-class ICNDbTest extends PHPUnit_Framework_TestCase {
+class ICNDbTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->wrapper = new Client;
+    }
 
-	public function setUp()
-	{
-		$this->wrapper = new Client;
-	}
+    public function testCanSetCustomConfig()
+    {
+        $config = array(
+            'firstName' => 'John',
+            'lastName' => 'Doe'
+        );
 
-	public function testCanSetCustomConfig()
-	{
-		$config = array(
-			'firstName' => 'John',
-			'lastName' => 'Doe'
-		);
+        $wrapper = new Client($config);
 
-		$wrapper = new Client($config);
+        $this->assertAttributeEquals($config, 'config', $wrapper);
+    }
 
-		$this->assertAttributeEquals($config, 'config', $wrapper);
-	}
+    public function testSetsCorrectUriForCategories()
+    {
+        $this->wrapper->categories();
+        $this->assertAttributeEquals('categories', 'uri', $this->wrapper);
+    }
 
-	public function testSetsCorrectUriForCategories()
-	{
-		$this->wrapper->categories();
-		$this->assertAttributeEquals('categories', 'uri', $this->wrapper);
-	}
+    public function testSetsCorrectUriForRandomJoke()
+    {
+        $this->wrapper->random();
+        $this->assertAttributeEquals('jokes/random/1', 'uri', $this->wrapper);
 
-	public function testSetsCorrectUriForRandomJoke()
-	{
-		$this->wrapper->random();
-		$this->assertAttributeEquals('jokes/random/1', 'uri', $this->wrapper);
+        $this->wrapper->random(2);
+        $this->assertAttributeEquals('jokes/random/2', 'uri', $this->wrapper);
+    }
 
-		$this->wrapper->random(2);
-		$this->assertAttributeEquals('jokes/random/2', 'uri', $this->wrapper);
-	}
+    public function testSetsCorrectUriForSpecificJoke()
+    {
+        $this->wrapper->specific(10);
+        $this->assertAttributeEquals('jokes/10', 'uri', $this->wrapper);
+    }
 
-	public function testSetsCorrectUriForSpecificJoke()
-	{
-		$this->wrapper->specific(10);
-		$this->assertAttributeEquals('jokes/10', 'uri', $this->wrapper);
-	}
+    /**
+     * @expectedException LogicException
+     */
+    public function testShouldNotChainSpecificAndRandom()
+    {
+        // You can't get a random, and a specific joke all at the same time
+        $this->wrapper->random()->specific(1)->get();
+    }
 
-	/**
-	 * @expectedException LogicException
-	 */
-	public function testShouldNotChainSpecificAndRandom()
-	{
-		// You can't get a random, and a specific joke all at the same time
-		$this->wrapper->random()->specific(1)->get();
-	}
+    /**
+     * @expectedException LogicException
+     */
+    public function testShouldNotChainJokeAndCategories()
+    {
+        // You can't get a random, and a specific joke all at the same time
+        $this->wrapper->specific(1)->categories()->get();
+    }
 
-	/**
-	 * @expectedException LogicException
-	 */
-	public function testShouldNotChainJokeAndCategories()
-	{
-		// You can't get a random, and a specific joke all at the same time
-		$this->wrapper->specific(1)->categories()->get();
-	}
+    public function testFirstMethod()
+    {
+        $item = array(
+            'id' => 1,
+            'joke' => 'foo',
+            'categories' => array()
+        );
 
-	public function testFirstMethod()
-	{
-		$item = array(
-			'id' => 1,
-			'joke' => 'foo',
-			'categories' => array()
-		);
+        $mock = $this->getMock('ICNDb\Client', array('get'));
+        $mock->expects($this->once())
+             ->method('get')
+             ->will($this->returnValue(array($item)));
 
-		$mock = $this->getMock('ICNDb\Client', array('get'));
-		$mock->expects($this->once())
-			 ->method('get')
-			 ->will($this->returnValue(array($item)));
-
- 		$this->assertEquals($item, $mock->random()->first());
-	}
+        $this->assertEquals($item, $mock->random()->first());
+    }
 }
